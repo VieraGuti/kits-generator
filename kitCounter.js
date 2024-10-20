8c9f40c7cf3cb0472dcc029671f0324bdc044c6a
VieraGuti <angelviera99@gmail.com>
2024-10-20 13:23:58 +0100

    Update kitCounter.js

--- kitCounter.js
+++ kitCounter.js
@@ -1,13 +1,13 @@
 // kitCounter.js
 
-let kitCount = localStorage.getItem('kitCount') ? parseInt(localStorage.getItem('kitCount')) : 0; // Inicializa el contador
+let kitCount = localStorage.getItem('kitCount') ? parseInt(localStorage.getItem('kitCount')) : 0; 
 document.addEventListener('DOMContentLoaded', () => {
-    document.getElementById('kitCounter').innerText = kitCount; // Muestra el contador al cargar
+    document.getElementById('kitCounter').innerText = kitCount; 
 
-    // Función para incrementar el contador
+    
     window.incrementKitCounter = function() {
         kitCount++;
-        localStorage.setItem('kitCount', kitCount); // Guarda el nuevo contador en localStorage
-        document.getElementById('kitCounter').innerText = kitCount; // Actualiza el contador en la UI
+        localStorage.setItem('kitCount', kitCount); 
+        document.getElementById('kitCounter').innerText = kitCount; 
     };
 });
